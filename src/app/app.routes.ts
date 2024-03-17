ce98804209951e44bb8e21496106b8588a17b40e
Jan-Frederic Overbeck <jan-frederic.overbeck@flender.com>
2024-03-17 23:18:53 +0100

    tree

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { TreeDynamicExampleComponent } from './tree-dynamic-example/tree-dynamic-example.component';
 
 export const routes: Routes = [
-    { path: 'tree', component: TreeDynamicExampleComponent
- },
+    {path: '', redirectTo: '/tree', pathMatch: 'full'},
+    { path: 'tree', component: TreeDynamicExampleComponent},
 ];
