abb7ec8de5f028431f7dbf6f09080b5ce909a48d
Jan-Frederic Overbeck <jan-frederic.overbeck@flender.com>
2024-03-18 13:43:28 +0100

    sausage

--- src/app/tree-dynamic-example/tree-dynamic-example.component.ts
+++ src/app/tree-dynamic-example/tree-dynamic-example.component.ts
@@ -25,7 +25,7 @@ export class DynamicFlatNode {
 @Injectable({providedIn: 'root'})
 export class DynamicDatabase {
   dataMap = new Map<string, string[]>([
-    ['Fruits', ['Apple', 'Orange', 'Banana']],
+    ['Fruits', ['Apple', 'Orange', 'Banana', 'Sausage']],
     ['Vegetables', ['Tomato', 'Potato', 'Onion']],
     ['Apple', ['Fuji', 'Macintosh']],
     ['Onion', ['Yellow', 'White', 'Purple']],
